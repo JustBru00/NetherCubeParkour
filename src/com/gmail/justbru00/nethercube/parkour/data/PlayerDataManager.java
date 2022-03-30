6fec4a187f0d5f4cecb01feaa87ba03b93c76300
JustBru00 <justbru00@gmail.com>
2022-03-29 20:02:18 -0400

    Continued working on updated player data storage system.

--- src/com/gmail/justbru00/nethercube/parkour/data/PlayerDataManager.java
+++ src/com/gmail/justbru00/nethercube/parkour/data/PlayerDataManager.java
@@ -38,111 +38,234 @@ public class PlayerDataManager {
 				});
 			}
 		}
+	}	
+	
+	public static synchronized void savePlayerDataAsync(PlayerData pd) {
+		// Save to disk async.
+		scheduler.runTaskAsynchronously(NetherCubeParkour.getInstance(), new Runnable() {
+			
+			@Override
+			public void run() {				
+				File playersDataFile = new File(NetherCubeParkour.getInstance().getDataFolder().getPath() + 
+						File.separator + "flatfilestorage" + File.separator + pd.getUuid().toString() + ".yml");
+				
+				// Personal Data File
+				PluginFile pdf = new PluginFile(NetherCubeParkour.getInstance(), playersDataFile);
+				
+				// We ignore the lastname value as we don't know if the player is online. `lastname` is more for confirming that an admin is editing the right file.
+				pdf.set("currency", pd.getCurrency());
+				// Map data storage keys are abbreviated to save storage space for large amounts of players.
+				// u = unlocked, a = attempts, f = finishes, b = besttime					
+				
+				for (PlayerMapData pmd : pd.getMapData()) {					
+					pdf.set("maps." + pmd.getInternalName() + ".u", pmd.isUnlocked());					
+					pdf.set("maps." + pmd.getInternalName() + ".a", pmd.getAttempts());
+					pdf.set("maps." + pmd.getInternalName() + ".f", pmd.getFinishes());
+					pdf.set("maps." + pmd.getInternalName() + ".b", pmd.getBestTime());
+				}
+				pdf.save();
+			}
+		});		
+	}
+	
+	/**
+	 * This method attempts to get the requested PlayerData from the built in cache.
+	 * If it isn't in the cache, then we load it in sync with the calling process
+	 * @param p
+	 * @return
+	 */
+	public static PlayerData getPlayerData(OfflinePlayer p) {
+		PlayerData matchingData = null;
+		for (PlayerData pd : dataForOnlinePlayers) {
+			if (pd.getUuid().equals(p.getUniqueId())) {
+				matchingData = pd;
+			}
+		}
+		
+		if (matchingData == null) {
+			// The requested player isn't online.
+			return getPlayerDataSync(p);
+		}
+		
+		return matchingData;
+	}
+	
+	/**
+	 * Call this method when a player joins.
+	 * @param player
+	 */
+	public static void onPlayerJoin(Player player) {
+		getPlayerDataAsyncAndRunCallback(Bukkit.getOfflinePlayer(player.getUniqueId()), new PlayerDataCallback() {
+			
+			@Override
+			public void inSync(PlayerData playerData) {
+				dataForOnlinePlayers.add(playerData);				
+			}
+		});
+	}
+	
+	/**
+	 * Call this method every five minutes or so
+	 * This method clears the cache of players that are no longer online.
+	 */
+	public static void runEveryFiveMinutes() {
+		for (PlayerData pd : dataForOnlinePlayers) {
+			if (!Bukkit.getOfflinePlayer(pd.getUuid()).isOnline()) {
+				dataForOnlinePlayers.remove(pd);
+			}
+		}
 	}
 	
 	private static void migrateV1ToV2(OfflinePlayer offlinePlayer) {
-		// TODO Check if this server still has an old data.yml file.
+		// Check if this server still has an old data.yml file.
 		// If it does we need to transfer the old data format to the new format.
 		// If a players data has already been migrated the players UUID section in the data.yml file will simply be a string
 		// with "Migrated To V2" Ex. 28f9bb08-b33c-4a7d-b098-ebf271383966: Migrated To V2
 		File dataYml = new File(NetherCubeParkour.getInstance().getDataFolder().getPath() + 
 				File.separator + "data.yml");
-		if (dataYml.exists()) {			
-			PluginFile dataFile = new PluginFile(NetherCubeParkour.getInstance(), dataYml);
+		if (!dataYml.exists()) {			
+			// No data.yml file, no data to migrate.
+			return;
+		}	
+		
+		PluginFile dataFile = new PluginFile(NetherCubeParkour.getInstance(), dataYml);
 			
-			if (dataFile.getConfigurationSection(offlinePlayer.getUniqueId().toString()) != null) {
-				// Section exists for this player
-				if (!dataFile.getString(offlinePlayer.getUniqueId().toString()).equalsIgnoreCase("Migrated To V2")) {
-					// We need to migrate this players data.
-					Messager.msgConsole("&6[PlayerDataManager] Attempting to migrate player data for " + offlinePlayer.getUniqueId() + " from v1 to v2.");
-					// TODO Load this data from dataFile and save it to a new /NetherCubeParkour/flatfilestorage/uuid.yml file.
-				}				
-			}
+		if (dataFile.getConfigurationSection(offlinePlayer.getUniqueId().toString()) != null) {
+			// Section exists for this player
+			if (!dataFile.getString(offlinePlayer.getUniqueId().toString()).equalsIgnoreCase("Migrated To V2")) {
+				// We need to migrate this players data.
+				Messager.msgConsole("&6[PlayerDataManager] Attempting to migrate player data for " + offlinePlayer.getUniqueId() + " from v1 to v2.");
+				// Load this data from dataFile and save it to a new /NetherCubeParkour/flatfilestorage/uuid.yml file.
+				String pathPre = offlinePlayer.getUniqueId().toString() + ".";
+				
+				int currency = dataFile.getInt(pathPre + "currency");
+				
+				ArrayList<PlayerMapData> mapDataList = new ArrayList<PlayerMapData>();
+				
+				for (String mapKey : dataFile.getConfigurationSection(pathPre + "maps").getKeys(false)) {
+					boolean unlocked = dataFile.getBoolean(pathPre + "map." + mapKey + ".unlocked");
+					int attempts = dataFile.getInt(pathPre + "map." + mapKey + ".attempts");
+					int finishes = dataFile.getInt(pathPre + "map." + mapKey + ".finishes");
+					long besttime = dataFile.getLong(pathPre + "map." + mapKey + ".besttime");
+					
+					PlayerMapData aMapsData = new PlayerMapData(mapKey, unlocked, attempts, finishes, besttime);
+					mapDataList.add(aMapsData);
+				}
+				
+				// Write to the new file format
+				File playersDataFile = new File(NetherCubeParkour.getInstance().getDataFolder().getPath() + 
+						File.separator + "flatfilestorage" + File.separator + offlinePlayer.getUniqueId().toString() + ".yml");
+				
+				// Personal Data File
+				PluginFile pdf = new PluginFile(NetherCubeParkour.getInstance(), playersDataFile);
+				
+				pdf.set("lastname", offlinePlayer.getName());
+				pdf.set("currency", currency);
+				// Map data storage keys are abbreviated to save storage space for large amounts of players.
+				// u = unlocked, a = attempts, f = finishes, b = besttime					
+				
+				for (PlayerMapData pmd : mapDataList) {					
+					pdf.set("maps." + pmd.getInternalName() + ".u", pmd.isUnlocked());					
+					pdf.set("maps." + pmd.getInternalName() + ".a", pmd.getAttempts());
+					pdf.set("maps." + pmd.getInternalName() + ".f", pmd.getFinishes());
+					pdf.set("maps." + pmd.getInternalName() + ".b", pmd.getBestTime());
+				}
+				pdf.save();
+				
+				dataFile.set(offlinePlayer.getUniqueId().toString(), "Migrated To V2");
+				dataFile.save();
+			}				
 		}
+		
+	}
+	
+	private static PlayerData getPlayerDataSync(final OfflinePlayer offlinePlayer) {
+		migrateV1ToV2(offlinePlayer);
+		
+		// Load or create the player data from yaml files located in /NetherCubeParkour/flatfilestorage/uuid.yml
+		final PlayerData playerData = new PlayerData(offlinePlayer.getUniqueId());
+		
+		File playersDataFile = new File(NetherCubeParkour.getInstance().getDataFolder().getPath() + 
+				File.separator + "flatfilestorage" + File.separator + offlinePlayer.getUniqueId().toString() + ".yml");
+		
+		// Personal Data File
+		PluginFile pdf = new PluginFile(NetherCubeParkour.getInstance(), playersDataFile);
+		
+		// If the file is empty we need to fill it with default data.
+		if (pdf.getString("lastname") == null) {
+			// This file is empty and it needs to be filled.
+			pdf.set("lastname", offlinePlayer.getName());
+			pdf.set("currency", 0);
+			// Map data storage keys are abbreviated to save storage space for large amounts of players.
+			// u = unlocked
+			// a = attempts
+			// f = finishes
+			// b = besttime					
+			
+			for (Map map : MapManager.getMaps()) {
+				if (map.getPurchaseCost() == Map.UNLOCKED_DEFAULT) {
+					pdf.set("maps." + map.getInternalName() + ".u", true);
+				} else {
+					pdf.set("maps." + map.getInternalName() + ".u", false);
+				}
+				
+				pdf.set("maps." + map.getInternalName() + ".a", 0);
+				pdf.set("maps." + map.getInternalName() + ".f", 0);
+				pdf.set("maps." + map.getInternalName() + ".b", (long) -1);
+			}
+			pdf.save();
+		}
+		
+		// Load the values from the file now that we are sure at least something exists.
+		playerData.setCurrency(pdf.getInt("currency"));
+		
+		Set<String> mapKeys = pdf.getConfigurationSection("maps").getKeys(false);
+		
+		ArrayList<PlayerMapData> playerMapData = new ArrayList<PlayerMapData>();
+		
+		for (Map map : MapManager.getMaps()) {
+			if (mapKeys.contains(map.getInternalName())) {
+				// Load the data for this map
+				PlayerMapData mapData = new PlayerMapData(map.getInternalName());
+				
+				mapData.setUnlocked(pdf.getBoolean("maps." + map.getInternalName() + ".u"));
+				mapData.setAttempts(pdf.getInt("maps." + map.getInternalName() + ".a"));
+				mapData.setFinishes(pdf.getInt("maps." + map.getInternalName() + ".f"));
+				mapData.setBestTime(pdf.getLong("maps." + map.getInternalName() + ".b"));
+				
+				playerMapData.add(mapData);
+			} else {
+				// Missing the section for this map. We need to add it.
+				boolean unlocked;
+				if (map.getPurchaseCost() == Map.UNLOCKED_DEFAULT) {
+					pdf.set("maps." + map.getInternalName() + ".u", true);
+					unlocked = true;
+				} else {
+					pdf.set("maps." + map.getInternalName() + ".u", false);
+					unlocked = false;
+				}
+				
+				pdf.set("maps." + map.getInternalName() + ".a", 0);
+				pdf.set("maps." + map.getInternalName() + ".f", 0);
+				pdf.set("maps." + map.getInternalName() + ".b", (long) -1);
+				pdf.save();
+				playerMapData.add(new PlayerMapData(map.getInternalName(), unlocked, 0, 0, -1));
+			}						
+		}
+		
+		playerData.setMapData(playerMapData);
+		return playerData;
 	}
 	
 	private static void getPlayerDataAsyncAndRunCallback(final OfflinePlayer offlinePlayer, final PlayerDataCallback callback) {
 		scheduler.runTaskAsynchronously(NetherCubeParkour.getInstance(), new Runnable() {			
 			@Override
-			public void run() {				
-				migrateV1ToV2(offlinePlayer);
+			public void run() {			
 				
-				// Load or create the player data from yaml files located in /NetherCubeParkour/flatfilestorage/uuid.yml
-				final PlayerData playerData = new PlayerData(offlinePlayer.getUniqueId());
-				
-				File playersDataFile = new File(NetherCubeParkour.getInstance().getDataFolder().getPath() + 
-						File.separator + "flatfilestorage" + File.separator + offlinePlayer.getUniqueId() + ".yml");
-				
-				// Personal Data File
-				PluginFile pdf = new PluginFile(NetherCubeParkour.getInstance(), playersDataFile);
-				
-				// If the file is empty we need to fill it with default data.
-				if (pdf.getString("lastname") == null) {
-					// This file is empty and it needs to be filled.
-					pdf.set("lastname", offlinePlayer.getName());
-					pdf.set("currency", 0);
-					// Map data storage keys are abbreviated to save storage space for large amounts of players.
-					// u = unlocked
-					// a = attempts
-					// f = finishes
-					// b = besttime					
-					
-					for (Map map : MapManager.getMaps()) {
-						if (map.getPurchaseCost() == Map.UNLOCKED_DEFAULT) {
-							pdf.set("maps." + map.getInternalName() + ".u", true);
-						} else {
-							pdf.set("maps." + map.getInternalName() + ".u", false);
-						}
-						
-						pdf.set("maps." + map.getInternalName() + ".a", 0);
-						pdf.set("maps." + map.getInternalName() + ".f", 0);
-						pdf.set("maps." + map.getInternalName() + ".b", (long) -1);
-					}
-					pdf.save();
-				}
-				
-				// Load the values from the file now that we are sure at least something exists.
-				playerData.setCurrency(pdf.getInt("currency"));
-				
-				Set<String> mapKeys = pdf.getConfigurationSection("maps").getKeys(false);
-				
-				ArrayList<PlayerMapData> playerMapData = new ArrayList<PlayerMapData>();
-				
-				for (Map map : MapManager.getMaps()) {
-					if (mapKeys.contains(map.getInternalName())) {
-						// Load the data for this map
-						PlayerMapData mapData = new PlayerMapData(map.getInternalName());
-						
-						mapData.setUnlocked(pdf.getBoolean("maps." + map.getInternalName() + ".u"));
-						mapData.setAttempts(pdf.getInt("maps." + map.getInternalName() + ".a"));
-						mapData.setFinishes(pdf.getInt("maps." + map.getInternalName() + ".f"));
-						mapData.setBestTime(pdf.getLong("maps." + map.getInternalName() + ".b"));
-						
-						playerMapData.add(mapData);
-					} else {
-						// Missing the section for this map. We need to add it.
-						boolean unlocked;
-						if (map.getPurchaseCost() == Map.UNLOCKED_DEFAULT) {
-							pdf.set("maps." + map.getInternalName() + ".u", true);
-							unlocked = true;
-						} else {
-							pdf.set("maps." + map.getInternalName() + ".u", false);
-							unlocked = false;
-						}
-						
-						pdf.set("maps." + map.getInternalName() + ".a", 0);
-						pdf.set("maps." + map.getInternalName() + ".f", 0);
-						pdf.set("maps." + map.getInternalName() + ".b", (long) -1);
-						pdf.save();
-						playerMapData.add(new PlayerMapData(map.getInternalName(), unlocked, 0, 0, -1));
-					}						
-				}
-				
-				playerData.setMapData(playerMapData);
 				
 				// Pass the loaded PlayerData object to the callback.
 				scheduler.runTask(NetherCubeParkour.getInstance(), new Runnable() {
-					
+					PlayerData playerData = getPlayerDataSync(offlinePlayer);
 					@Override
 					public void run() {
 						callback.inSync(playerData);						
